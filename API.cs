fdba19adb081cf3ec790dc2c7ef222b9ee8a6b20
pirilya <svalgreen+tincosanar@gmail.com>
2022-10-06 09:33:58 +0200

    Minor changes to the server code

--- API.cs
+++ API.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Net;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Cardgame.Blackjack {
     public class APIServer {
@@ -13,10 +14,19 @@ namespace Cardgame.Blackjack {
             Listener.Start();
             Console.WriteLine("Listening for connections on port {0}", Port);
         }
+        void DebugPrint(HttpListenerRequest request) {
+            Console.WriteLine("request! at url path {0}, with query {1}, headers:\n{2}", 
+            request.Url.AbsolutePath, request.Url.Query, request.Headers);
+            string text;
+            using (var reader = new StreamReader(request.InputStream, request.ContentEncoding)) {
+                text = reader.ReadToEnd();
+            }
+            Console.WriteLine("the request body is ", text);
+        }
         public async Task Listen() {
             while (true) {
                 var context = await Listener.GetContextAsync();
-                Console.WriteLine("request! at url {0}", context.Request.Url);
+                DebugPrint(context.Request);
                 context.Response.OutputStream.Write(new byte[] {}, 0, 0);
                 context.Response.OutputStream.Close();
             }
--- MainRunner.cs
+++ MainRunner.cs
@@ -1,15 +1,14 @@
 using System;
+using System.Threading.Tasks;
 
 namespace Cardgame.Blackjack {
     class MainRunner {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
             var CLI = new CommandLineInterface();
-            CLI.Run();
-            //var Server = new APIServer(8000);
-            // Note: This is a hacky way to call an async function, 
-            // consider looking up what the approved non-hacky way is
-            //Server.Listen().GetAwaiter().GetResult(); 
+            //CLI.Run();
+            var Server = new APIServer(8000);
+            await Server.Listen();
         }
     }
 }
